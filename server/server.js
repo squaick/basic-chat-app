61321c360d6fa60c061919ed34d6866b6860a352
Suleyman Kalkan <skalkan.dev@gmail.com>
2021-06-15 23:54:20 +0300

    Message end disconnect event is ready.

--- server/server.js
+++ server/server.js
@@ -3,25 +3,33 @@ const io = require('socket.io')(3000, {cors: {origin: "*",},});
 const {addUser, removeUser, getUser} = require('./users');
 
 io.on('connection', socket => {
-    // Emit on new user joined
-    console.log("New connection!");
 
+    // User join event
     socket.on('new-user', ({name}, callback) => {
+      //Check if username exists, if not return user obj.
       const { error, user } = addUser({ id: socket.id, name});
 
       if(error) return callback(error);
 
-      console.log("New user! name: " + user.name);
-      socket.broadcast.emit('user-connected', name)
-    })
+      socket.broadcast.emit('user-connected', name);
 
-    socket.on('send-chat-message', message => {
-      socket.broadcast.emit('chat-message', { message: message})
-    })
+      callback();
+    });
+
+    // Message event
+    socket.on('send-chat-message', (message, callback) => {
+      const user = getUser(socket.id);
+
+      socket.broadcast.emit('chat-message', { user: user.name, message: message});
+
+      callback();
+    });
+
 
     socket.on('disconnect', () => {
-      socket.broadcast.emit('user-disconnected' )
-      // removeUser(..) to remove the user from array
-      console.log("User disconnected");
-    })
+      const user = removeUser(socket.id);
+
+      socket.broadcast.emit('user-disconnected' , user.name);
+
+    });
   })
