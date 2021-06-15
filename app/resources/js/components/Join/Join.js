17ca771e78687b0c6b7f7795b74e5bd5fdca05f5
Suleyman Kalkan <skalkan.dev@gmail.com>
2021-06-15 12:40:24 +0300

    Styling of Join component.

--- app/resources/js/components/Join/Join.js
+++ app/resources/js/components/Join/Join.js
@@ -1,10 +1,19 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import './Join.css';
 
 const Join = () => {
+    const [name, setName] = useState('');
+
+
     return (
-        <div>
-            This is join.
-        </div>
+    <div className="joinContainer">
+        <p className="sign" align="center">Basic Chat App</p>
+        <input className="un" type="text" align="center" placeholder="Username"/>
+        <button class="submitJoin" align="center">Enter Chat</button>
+    </div>
     )
 }
 
-export default Join
+export default Join;
